763746242e50b10da887181e7a81da833f9d54e6
The_real_KING <vlad.power01@gmail.com>
2023-01-28 11:44:19 +0100

    add game sprite

--- shooter.py
+++ shooter.py
@@ -14,11 +14,27 @@ def file_path(file_name):
 image_background = pygame.image.load(file_path("fon.jpg"))
 image_background = pygame.transform.scale(image_background,(WIN_WIDTH, WIN_HEIGHT))
 
+Player_image = pygame.image.load(file_path("fon.jpg"))
+Player_image = pygame.transform.scale(Player_image,(50, 50))
+
+enemy_image = pygame.image.load(file_path("fon.jpg"))
+enemy_image = pygame.transform.scale(enemy_image,(50, 50))
 
 window = pygame.display.set_mode([WIN_WIDTH,WIN_HEIGHT])
 clock = pygame.time.Clock()
 
+class GameSprite(pygame.sprite.Sprite):
+    def __init__ (self,x,y,wwidth,height,img,speed):
+        super().__init__()
+        self.image = pygame.imge.load(file_path(img))
+        self.image = pygame.transform.scale(self.image(width,height))
+        self.rect = self.image.get_rect()
+        self.rect.x = x 
+        self.rect.y = y
+        self.speed = speed
 
+    def reset(self):
+        window.blit(self.image,(self.rect.x,self.rect.y))
 
 play = True 
 game = True 
